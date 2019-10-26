d8c0ca5f6f91774872487a6c1ee6d786b5bde5cb
hencel <zbigniewpiernik@gmail.com>
2019-10-26 23:48:27 +0200

    Gallery of the images - input data from user

--- js/app.js
+++ js/app.js
@@ -36,6 +36,21 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(dayImage);
       img.src = dayImage;
     })
+  
+  const search = document.querySelector('input[name="searchButton"]');
+  let userData;
+  search.addEventListener('click', search => {
+    userData = document.querySelector('input[name="searchArea"]').value.toString();
+    console.log(userData);
+    console.log(typeof userData);
+    fetch(`https://images-api.nasa.gov/search?q=${userData}`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+
+    //pobierz wpisany tekst i umieść w fetch poniżej zamiast apollo
+    
+    //uzyskaj tylko zdjęcia data.collection.items[0].data[0].media_type != video
+    //stwórz galerię przez forEach dla np.50 zdjęć
+  })
+
 });
-
-
